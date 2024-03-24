1046f91e4388264f5af551ac61fcc56e54634b53
cevher.bozkur <bozkur@gmail.com>
2024-03-24 12:59:31 +0300

    Initialization.

---
+++ src/test/java/org/sfg/brewery/msscconfigserver/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sfg.brewery.msscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
